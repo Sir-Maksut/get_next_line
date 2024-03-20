7b63ccf506b5d27691aed67c5cada5efd3b69c5e
Sir-Maksut <makokcmn@gmail.com>
2024-03-21 00:01:31 +0300

    get_next_line
    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkocaman <mkocaman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 16:01:57 by mkocaman          #+#    #+#             */
+/*   Updated: 2023/08/12 12:07:58 by mkocaman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*read_function(int fd, char *str);
+char	*ft_getline(char *fullStr);
+char	*ft_getrest(char *fullStr);
+
+#endif
